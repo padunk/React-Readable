e3f68b646c8fb36ff369d594250e5ddfdb14d9a7
Padunk <abraham.agel@gmail.com>
2020-05-07 17:36:36 +0700

    change display date

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,13 +1,31 @@
-
 export const timeConverter = (UNIX_timestamp) => {
-  let a = new Date(UNIX_timestamp);
-  let months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-  let year = a.getFullYear();
-  let month = months[a.getMonth()];
-  let date = a.getDate();
-  let hour = a.getHours();
-  let min = a.getMinutes() < 10 ? '0' + a.getMinutes() : a.getMinutes();
-  let sec = a.getSeconds() < 10 ? '0' + a.getSeconds() : a.getSeconds();
-  let time = `${date}-${month}-${year} Time: ${hour}:${min}:${sec}`;
+  let createDate = new Date(UNIX_timestamp);
+  let now = Date.now();
+  let diff = Math.floor((now - createDate) / 1000); // diff in seconds
+  let timeInSeconds = {
+    mins: 60,
+    hours: 3600,
+    days: 86400,
+    weeks: 604800,
+    months: 2592000,
+    years: 31536000,
+  };
+  let time = "";
+
+  if (diff < timeInSeconds.mins) {
+    time = diff + "s";
+  } else if (diff < timeInSeconds.hours) {
+    time = `${Math.floor(diff / 60)}m ${diff % 60}s`;
+  } else if (diff < timeInSeconds.days) {
+    time = `${Math.floor(diff / 3600)}h ${Math.floor((diff / 60) % 60)}m`;
+  } else if (diff < timeInSeconds.weeks) {
+    time = Math.floor(diff / timeInSeconds.days) + " days ago";
+  } else if (diff < timeInSeconds.months) {
+    time = Math.floor(diff / timeInSeconds.weeks) + " weeks ago";
+  } else if (diff < timeInSeconds.months) {
+    time = Math.floor(diff / timeInSeconds.months) + " months ago";
+  } else {
+    time = Math.floor(diff / timeInSeconds.years) + " years ago";
+  }
   return time;
 };
